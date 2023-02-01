2b5008ce1b4bad175bd1db7cf932b995fe68aacc
matilde-silva-21 <matilde.m.silva.21@gmail.com>
2023-02-01 22:18:38 -0100

    adicionar mini-testes

--- prolog/ficha5/ex6.pl
+++ prolog/ficha5/ex6.pl
@@ -5,4 +5,21 @@ exists_in(Elem, List) :- member(Elem, List).
 
 /* (b) */
 
-:-op(500, xf, append).
+_Y as _X:- true.
+
+:-op(550, xfx, de).
+:-op(500, fx, aula).
+:-op(550, xfy, pelas).
+:-op(550, xfx, e).
+:-op(600, xfx, as).
+
+
+dfs(S, F, Nodes):-
+    dfs(S, F, [S], Help),
+    reverse(Help, Nodes).
+
+dfs(F, F, H, H).
+dfs(S, F, Nodes, H):-
+    edge(S, Next),
+    \+(member(Next, Nodes)),
+    dfs(Next, F, [Next|Nodes], H).
